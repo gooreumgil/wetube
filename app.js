954056be7327ea0cf63b0cdba1b075f3010db2b8
Gwanseok Lee <gooreumgil47@gmail.com>
2019-04-16 22:53:47 +0900

    2019.04.16

---
+++ app.js
@@ -0,0 +1,27 @@
+
+import express from "express";
+import morgan from "morgan"; // middelware
+import helmet from "helmet"; // middelware
+import cookieParser from "cookie-parser"; // middelware
+import bodyParser from "body-parser"; // middelware
+import { localsMiddleware } from "./middlewares";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
+const app = express();
+
+app.use(helmet());
+app.set("view engine", "pug");
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(localsMiddleware);
+
+
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
+
+export default app;
